db70592b7a182f20ac734dcd2bbfac0a27a99e0e
yoshiioko <acardoza@pdx.edu>
2022-02-23 08:56:36 -0800

    define componentDidMount() to get manager from contract and set to state.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { manager: "" };
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+
+    this.setState({ manager });
+  }
+
   render() {
     console.log(web3.version);
 
